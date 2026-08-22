d55aa35377374be5d28ab6686034cdfd54f67aa4
wonoong <dnd5301@gmail.com>
2026-08-23 05:43:31 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/stayon/stayon_backend/StayonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stayon.stayon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stayon/stayon_backend/StayonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayon.stayon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
