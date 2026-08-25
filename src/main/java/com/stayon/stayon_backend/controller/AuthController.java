a48be883b7c91ab0db7e40ec6a375c2e8e43b912
wonoong <dnd5301@gmail.com>
2026-08-26 06:39:26 +0900

    feat: implement logout

--- src/main/java/com/stayon/stayon_backend/controller/AuthController.java
+++ src/main/java/com/stayon/stayon_backend/controller/AuthController.java
@@ -9,7 +9,9 @@ import com.stayon.stayon_backend.service.AuthService;
 import com.stayon.stayon_backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +43,10 @@ public class AuthController {
         return ResponseEntity.ok(
                 authService.refresh(dto)
         );
+    }
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal Long userId) {
+        authService.logout(userId);
+        return ResponseEntity.ok().build();
     }
 }
